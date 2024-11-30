de678927865da35ab2c62fc9ef59d29eee2eb4bf
Daniel Marmelo <daniel.marmelo@hotmail.com>
2024-11-30 22:16:12 +0000

    add some utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -4,6 +4,11 @@ import kotlin.io.path.Path
 import kotlin.io.path.readText
 
 /**
+ * Reads all text from the given input txt file.
+ */
+fun readInputRaw(name: String) = Path("src/$name.txt").readText()
+
+/**
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
@@ -19,3 +24,12 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun Sequence<Int>.product(): Int =
+    reduce { acc, item -> acc * item }
+
+fun Iterable<Int>.product(): Int =
+    reduce { acc, item -> acc * item }
+
+fun Iterable<Long>.product(): Long =
+    reduce { acc, item -> acc * item }
