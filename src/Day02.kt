858a41182e38dc0161b6208714a8024f0c34815b
Daniel Marmelo <daniel.marmelo@hotmail.com>
2024-12-04 18:29:19 +0000

    day 2 refactor

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,42 +2,38 @@ fun main() {
 
     fun List<String>.parseInput() = map { it.split(" ").map { it.toInt() } }
 
-    fun List<Int>.isSafe(): Boolean {
-        val diffs = zipWithNext().map { it.second - it.first }
-        return diffs.all { it > 0 && it <= 3 } || diffs.all { it < 0 && it >= -3 }
-    }
+    fun isReportSafe(levels: List<Int>) = levels.zipWithNext { a, b -> b - a }
+        .let { slope -> slope.all { it in 1..3 } || slope.all { it in -3..-1 } }
 
     fun part1(input: List<List<Int>>): Int {
-        return input.count { it.isSafe() }
+        return input.count(::isReportSafe)
     }
 
-    fun List<Int>.toleratedLevels() = sequence<List<Int>> {
-        repeat(size) { index ->
-            yield(take(index) + drop(index + 1))
-        }
+    fun isReportSafeDampened(levels: List<Int>) = levels.indices.any { index ->
+        isReportSafe(levels.toMutableList().apply { removeAt(index) })
     }
 
     fun part2(input: List<List<Int>>): Int {
-        return input.count { it.isSafe() || it.toleratedLevels().any { it.isSafe() } }
+        return input.count { isReportSafe(it) || isReportSafeDampened(it) }
     }
 
     // Test if implementation meets criteria from the description, like:
     val testInput = """
-            7 6 4 2 1
-            1 2 7 8 9
-            9 7 6 2 1
-            1 3 2 4 5
-            8 6 4 4 1
-            1 3 6 7 9
-            """.trimIndent().lines().parseInput()
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    """.trimIndent().lines().parseInput()
     check(part1(testInput) == 2)
     check(part2(testInput) == 4)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    //val testInput = readInput("Day01_test")
+    // Or read a large test input from the `src/Day02_test.txt` file:
+    //val testInput = readInput("Day02_test")
     //check(part1(testInput) == 1)
 
-    // Read the input from the `src/Day01.txt` file.
+    // Read the input from the `src/Day02.txt` file.
     val input = readInput("Day02").parseInput()
     part1(input).println()
     part2(input).println()
