93cb3ddcf16a08ba5d6118f8df7e357eec26ad81
Daniel Marmelo <daniel.marmelo@hotmail.com>
2024-12-04 00:04:01 +0000

    day 1 refactor

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,9 +1,10 @@
 import kotlin.math.abs
 
 fun main() {
-    fun List<String>.parteInput() = map { line ->
-        val (f, s) = line.split("   ")
-        f.toInt() to s.toInt()
+
+    fun List<String>.parseInput() = map { line ->
+        val (first, second) = line.split("   ")
+        first.toInt() to second.toInt()
     }.unzip()
 
     fun part1(input: Pair<List<Int>, List<Int>>): Int {
@@ -12,19 +13,19 @@ fun main() {
     }
 
     fun part2(input: Pair<List<Int>, List<Int>>): Int {
-        val secondEachCount = input.second.groupingBy { it }.eachCount()
-        return input.first.sumOf { it * (secondEachCount[it] ?: 0) }
+        val frequencies = input.second.groupingBy { it }.eachCount()
+        return input.first.sumOf { it * (frequencies[it] ?: 0) }
     }
 
     // Test if implementation meets criteria from the description, like:
-    val testInput = listOf(
-        "3   4",
-        "4   3",
-        "2   5",
-        "1   3",
-        "3   9",
-        "3   3"
-    ).parteInput()
+    val testInput = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    """.trimIndent().lines().parseInput()
     check(part1(testInput) == 11)
     check(part2(testInput) == 31)
 
@@ -33,7 +34,7 @@ fun main() {
     //check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01").parteInput()
+    val input = readInput("Day01").parseInput()
     part1(input).println()
     part2(input).println()
 }
